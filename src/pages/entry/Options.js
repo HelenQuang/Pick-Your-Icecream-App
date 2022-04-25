92fa3a1bc8b327783b44f9d568a4551d4b7cad42
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-04-25 22:33:49 +0300

    Test and code OrderEntry and AlertBanner

--- src/pages/entry/Options.js
+++ src/pages/entry/Options.js
@@ -3,17 +3,24 @@ import axios from "axios";
 import ScoopOptions from "./ScoopOptions";
 import ToppingOptions from "./ToppingOptions";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
-  //const [errors, setErrors] = useState(false);
+  const [errors, setErrors] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((res) => setItems(res.data))
-      .catch((error) => {});
+      .catch((error) => {
+        setErrors(true);
+      });
   }, [optionType]);
+
+  if (errors) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
   const item = items.map((item) => (
